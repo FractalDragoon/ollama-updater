db8a6bd02ddbaba5a3b36b6a2d5a240920ccaa24
Alejandro Arbelaez <aarbelaez@protonmail.com>
2026-04-30 08:20:21 -0500

    Initial commit

--- ollama-updater.sh
+++ ollama-updater.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
-
 echo "Updating Ollama..."
 curl -fsSL https://ollama.com/install.sh | sh
-
 echo "Updating models..."
 ollama list | awk 'NR>1{print $1}' | xargs -I {} sh -c 'echo "Updating model: {}"; ollama pull {}'
-
 echo "Done."
